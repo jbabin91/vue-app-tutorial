02def501fd659549db12cfa0d32269a75521b4bc
Jace Babin <jbabin91@gmail.com>
2018-08-02 16:45:24 -0700

    Getting started with vue.js

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'My first Vue app',
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: 400;
+  text-align: center;
+}
+</style>

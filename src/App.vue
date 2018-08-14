1b4cb6ac23b91e17efcd0153bd4ce45ca6516871
Jace Babin <jbabin91@gmail.com>
2018-08-14 14:23:49 -0700

    Base vue project for all of the features in the tutorial.

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,23 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar/>
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar';
-import AllFriends from './AllFriends';
-import OnlineFriends from './OnlineFriends';
 
 export default {
   name: 'app',
   components: {
     Navbar,
-    OnlineFriends,
-    AllFriends,
   },
   data() {
     return {
       title: 'My first Vue app',
-      friends: [
-        { name: 'Mario', online: true },
-        { name: 'Luigi', online: false },
-        { name: 'Toad', online: true },
-        { name: 'Bowser', online: false },
-      ],
     };
   },
-  methods: {
-    deleteFriend(payload) {
-      console.log(payload);
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name;
-      });
-    },
-  },
+  methods: {},
 };
 </script>
 

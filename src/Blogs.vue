f3adaeddab861e22855e61de375f2bd44e043cdd
Jace Babin <jbabin91@gmail.com>
2018-08-14 15:35:18 -0700

    Custom search box

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="blogs">
     <h2>Blogs</h2>
-    <div v-for="post in posts" :key="post.id">
+    <input type="text" v-model="searchTerm" />>
+    <div v-for="post in filteredPosts" :key="post.id">
       <h3>{{ post.title }}</h3>
       <p>{{ post.body | snippet }}</p>
     </div>
@@ -16,9 +17,17 @@ export default {
   data() {
     return {
       posts: [],
+      searchTerm: '',
     };
   },
   methods: {},
+  computed: {
+    filteredPosts() {
+      return this.posts.filter(post => {
+        return post.title.match(this.searchTerm);
+      });
+    },
+  },
   created() {
     axios
       .get('https://jsonplaceholder.typicode.com/posts/')

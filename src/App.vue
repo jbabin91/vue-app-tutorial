fa27e537dff59b34f168c47189e2c7ec2b9ab9da
Jace Babin <jbabin91@gmail.com>
2018-08-14 15:15:28 -0700

    Made a call with axios to get fake json data.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
-    <Navbar/>
+    <Navbar />
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar';
+import Blogs from './Blogs';
 
 export default {
   name: 'app',
   components: {
     Navbar,
+    Blogs,
   },
   data() {
     return {
---
+++ src/Blogs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="blogs">
+    <h2>Blogs</h2>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Blogs',
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {},
+  created() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts/')
+      .then(response => {
+        console.log(response);
+        this.posts = response.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+};
+</script>
+
